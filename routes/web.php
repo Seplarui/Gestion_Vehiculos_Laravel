fd240efbaaf67195602d5c355c1f78594140831b
Sebas Sebas <seplarui@gmail.com>
2017-02-12 14:23:44 -0800

    Comprobación Vistas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    
+    return "Pantalla Principal";
+});
+
+Route::get('/login', function(){
+    return "Login de usuario";
+});
+
+Route::get('/logout', function(){
+    return "Logout de usuario";
+});
+
+Route::get('/marcas', function(){
+    return "Listado de Marcas";
+});
+
+Route::get('/marcas/show/{id}', function(){
+    return "Detalles de marcas";
+});
+
+Route::get('/marcas/create', function(){
+    return "Crear Marca";
+});
+
+Route::get('/marcas/edit/{id}', function(){
+    return "Editar Marca";
 });
