cbca9eeb98468db9c6d1e61c4ee19c09a6d1670a
Sebas Sebas <seplarui@gmail.com>
2017-02-13 14:07:32 -0800

    Instalado Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,51 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     //return view('welcome');
-    
-    return "Pantalla Principal";
+    //return "Pantalla Principal";
+
+    return view('home');
 });
 
-Route::get('/login', function(){
-    return "Login de usuario";
+Route::get('/login', function() {
+    //return "Login de usuario";
+    return view('views.auth.login');
 });
 
-Route::get('/logout', function(){
+Route::get('/logout', function() {
     return "Logout de usuario";
 });
 
-Route::get('/marcas', function(){
-    return "Listado de Marcas";
+Route::get('/marcas', function() {
+    //return "Listado de Marcas";
+    
+    return view('marcas.index');
 });
 
-Route::get('/marcas/show/{id}', function(){
-    return "Detalles de marcas";
+Route::get('/marcas/show/{id}', function() {
+    //return "Detalles de marcas";
+    return view('marcas.show');
 });
 
-Route::get('/marcas/create', function(){
-    return "Crear Marca";
+Route::get('/marcas/create', function() {
+    //return "Crear Marca";
+    
+    return view('marcas.create');
 });
 
-Route::get('/marcas/edit/{id}', function(){
-    return "Editar Marca";
+Route::get('/marcas/edit/{id}', function() {
+    //return "Editar Marca";
+    
+    return view('marcas.edit');
 });
